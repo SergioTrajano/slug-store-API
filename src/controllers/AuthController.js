f2fcb23ade4722f7e58a2b97a706bf792fb7935c
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-11-28 12:59:39 -0300

    fix: change statusCode of POST /users responde to 201

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -11,15 +11,15 @@ export async function signUp(req, res) {
     const senhaHashada = hashSync(dadosUsuarios.senha, 13);
 
     try {
-        await db.collection(process.env.MONGO_CADASTRADOS).insertOne({...dadosUsuarios, senha: senhaHashada});
-        const usuario = await db.collection(process.env.MONGO_CADASTRADOS).findOne({ email: dadosUsuarios.email});
+        await db.collection(process.env.MONGO_CADASTRADOS).insertOne({ ...dadosUsuarios, senha: senhaHashada });
+        const usuario = await db.collection(process.env.MONGO_CADASTRADOS).findOne({ email: dadosUsuarios.email });
 
         await db.collection(process.env.MONGO_CARRINHOS).insertOne({
             carrinhoId: usuario._id,
             itens: [],
         });
 
-        res.sendStatus(200);
+        res.sendStatus(201);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
@@ -31,18 +31,18 @@ export async function signIn(req, res) {
     const dadosSessao = {
         email: usuario.email,
         sessionId: usuario._id,
-    }
-    
+    };
+
     try {
         await db.collection(process.env.MONGO_SESSIONS).insertOne(dadosSessao);
-        
+
         const chaveSecreta = process.env.JWT_SECRET;
         const token = jwt.sign(dadosSessao, chaveSecreta);
-        
+
         const dadosParaEnviar = {
             nome: usuario.nome,
             token,
-        }
+        };
 
         res.status(200).send(dadosParaEnviar);
     } catch (error) {
